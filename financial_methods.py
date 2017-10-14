ec9e1216d02763ba000f89c52b33e98392d4e311
jgerardsimcock <justin_simcock@yahoo.com>
2017-10-14 13:31:05 -0700

    add methods

---
+++ financial_methods.py
@@ -0,0 +1,102 @@
+
+
+
+def bsm_call_value(S0, K, T, r, sigma):
+    '''
+
+    Valuation of European call option in BSM model.
+    Analytical Formula
+
+    Parameters
+    ==========
+    S0 : float
+        initial stock/index level
+    K : float
+        strike price
+    T : float
+        maturity date (in year fractions)
+    r : float
+        constant risk-free short rate
+    sigma : float
+        volatility factor in diffusion term
+
+    Returns
+    =======
+    value : float
+        present value of European call option
+
+    '''
+
+    from numpy import log, sqrt, exp
+    from scipy import stats
+
+    S0 = float(S0)
+    d1 = (log(S0/K) + (r + 0.5 * sigma ** 2) * T)/ (sigma * sqrt(T))
+    d2 = (log(S0/K) + (r + 0.5 * sigma ** 2) * T)/ (sigma * sqrt(T))
+    value = (S0 * stats.norm.cdf(d1, 0.0, 1.0) - K )* exp(-r * T) * stats.norm.cdf(d2, 0.0, 1.0)
+
+    return value
+
+def bsm_vage(S0, K, T, r, sigma):
+    '''
+    Vega of European option in BSM Model
+
+    Parameters
+    ==========
+    S0 : float
+        initial stock/index level
+    K : float
+        strike price
+    T : float
+        maturity date (in year fractions)
+    r : float
+        constant risk-free short rate
+    sigma : float
+        volatility factor in diffusion term
+
+    Returns
+    =======
+    vega: float
+        partial derivative of BSM formula with respect to sigma
+    '''
+
+    from numpy import sqrt, log
+    from scipy import stats
+
+    S0 = float(S0)
+    d1 = (log(S0/K) + (r + 0.5 * sigma ** 2) * T)/ (sigma * sqrt(T))
+    vega = S0 * stats.norm.cdf(d1, 0.0, 1.0) *  sqrt(T)
+
+    return vega
+
+
+def bsm_call_imp_vol(S0, K, T, r, C0, sigma_est, it=100):
+    '''
+    Implied Volatility of European Call Option in BSM Model
+
+    Parameters
+    ==========
+    S0 : float
+        initial stock/index level
+    K : float
+        strike price
+    T : float
+        maturity date (in year fractions)
+    r : float
+        constant risk-free short rate
+    sigma_est: float
+        estimate of implied volatility
+    it : integer
+        number of iteration
+
+    Returns
+    =======
+    sigma_est: float
+        numericalluy estimated implied volatility
+    '''        
+
+    for i in range(it):
+        sigma_est -= ((bsm_call_value(S0, K, T, r,sigma_est) - C0) / bsm_vega(S0, K, T, r, sigma_est))
+
+
+
